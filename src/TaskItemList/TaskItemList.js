9597a95539528018b54d80cfa2b5f6e75fbc9fd2
youjung hong <dev.youjung.hong@gmail.com>
2020-05-24 15:03:47 +0900

    doc: add jsdoc descriptions & documentation

--- src/TaskItemList/TaskItemList.js
+++ src/TaskItemList/TaskItemList.js
@@ -1,5 +1,10 @@
 import { TaskItem } from '../TaskItem/TaskItem'
 
+/**
+ * @class
+ * @constructor
+ * @param {TaskItem[]} items - task items
+ */
 export class TaskItemList {
   constructor(items) {
     this.items = []
@@ -9,17 +14,29 @@ export class TaskItemList {
     }
   }
 
+  /**
+   * add an item to list
+   * @param {TaskItem} item - a task item
+   */
   add(item) {
     if (item instanceof TaskItem) {
       this.items.push(TaskItem.clone(item))
     }
   }
 
+  /**
+   * delete an item from list
+   * @param {number} removeId - an id of a task item which user wants to delete
+   */
   remove(removeId) {
     const id = removeId instanceof TaskItem ? removeId.id : removeId
     this.items = this.items.filter((item) => item.id !== id)
   }
 
+  /**
+   * update an existing item from list
+   * @param {TaskItem} updateItem - an item having new data that user wants to update
+   */
   update(updateItem) {
     this.items = this.items.map((item) => {
       if (item.id === updateItem.id) {
