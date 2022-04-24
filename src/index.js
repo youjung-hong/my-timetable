904734d3d0f2bdc641d69387668ab86a8b8cb041
youjung-hong <dev.youjung.hong@gmail.com>
2022-04-24 14:51:21 +0900

    Image Asset Management

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import _ from 'lodash';
 import './style.css';
+import Icon from './icon.png';
 
 function component() {
     const element = document.createElement('div');
@@ -7,6 +8,12 @@ function component() {
      // Lodash, now imported by this script
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
     element.classList.add('hello');
+
+    // Add the image to our existing div.
+    const myIcon = new Image();
+    myIcon.src = Icon;
+
+    element.appendChild(myIcon);
     
     return element;
   }
