492a4e3a9feb30e2434d44c72516f258f635c24c
youjung-hong <dev.youjung.hong@gmail.com>
2022-04-24 15:10:44 +0900

    Clean Up Tutorial Files

--- webpack.config.js
+++ webpack.config.js
@@ -5,21 +5,5 @@ module.exports = {
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
-  },
-  module: {
-    rules: [
-      {
-        test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
-      },
-      {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: 'asset/resource',
-      },
-      {
-        test: /\.(woff|woff2|eot|ttf|otf)$/i,
-        type: 'asset/resource',
-      },
-    ],
-  },
+  }
 };
