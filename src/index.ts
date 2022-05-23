ae7cfbe09a79fb97e71781361cbbc2cd2f30ebeb
youjung-hong <dev.youjung.hong@gmail.com>
2022-05-24 07:17:04 +0900

    TypeScript Config

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,5 @@
-import _ from 'lodash';
-
 function component() {
     const element = document.createElement('div');
-  
-     // Lodash, now imported by this script
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
     
     return element;
   }
