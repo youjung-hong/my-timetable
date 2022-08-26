49817d6ae5b87196be21f873881b2bd048df5528
Youjung Hong <dev.youjung.hong@gmail.com>
2022-08-26 22:42:14 +0900

    Develop (#50)
    * refactor: add eslint config
    
    * refactor: add prettier config
    
    * refactor: add husky config
    
    * chores: update package.json script
    
    * docs: update story link

---
+++ .prettierrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  tabWidth: 2,
+  useTabs: false,
+  printWidth: 80,
+  semi: false,
+  trailingComma: 'all',
+  singleQuote: true,
+}
--- src/templates.ts
+++ src/templates.ts
@@ -1,17 +1,18 @@
-import { Timeline } from "./Timeline";
+import { Timeline } from './Timeline'
 
-export const TIMETABLE_INNERHTML = (_startHour: number = 0) => {
-    const startHour = _startHour % 24;
-    const hourRange = [];
-    for (let i = 0; i < 24; i += 1) {
-        hourRange.push((startHour + i) % 24);
-    }
-    
-    return '<div class="timetable">' +
-        '<div class="hours hour-header">' +
-            hourRange.map(hour => `<div class="hour">${hour}</div>`).join('') +
-        '</div>' +
-        `<div class="contents">
+export const TIMETABLE_INNERHTML = (_startHour: number = 0): string => {
+  const startHour = _startHour % 24
+  const hourRange = []
+  for (let i = 0; i < 24; i += 1) {
+    hourRange.push((startHour + i) % 24)
+  }
+
+  return (
+    '<div class="timetable">' +
+    '<div class="hours hour-header">' +
+    hourRange.map((hour) => `<div class="hour">${hour}</div>`).join('') +
+    '</div>' +
+    `<div class="contents">
             <div class="minutes">
                 <div class="minute minute-0"></div>
                 <div class="minute minute-10"></div>
@@ -20,12 +21,19 @@ export const TIMETABLE_INNERHTML = (_startHour: number = 0) => {
                 <div class="minute minute-40"></div>
                 <div class="minute minute-50"></div>
             </div>` +
-            '<div class="hours">'+
-                hourRange.map(hour => `<div class="hour" data-hour="${hour}"></div>`).join('') +
-            '</div>' +
-        '</div>' +
-    '</div>';
+    '<div class="hours">' +
+    hourRange
+      .map((hour) => `<div class="hour" data-hour="${hour}"></div>`)
+      .join('') +
+    '</div>' +
+    '</div>' +
+    '</div>'
+  )
 }
 
-export const TIMELINE_INNERHTML = (timeline: Timeline, left: number | string, width: number | string) =>
-    `<div class="timeline" style="left: ${left}; width: ${width}; background-color: ${timeline.color};" data-timeline-id="${timeline.id}"></div>`
+export const TIMELINE_INNERHTML = (
+  timeline: Timeline,
+  left: number | string,
+  width: number | string,
+): string =>
+  `<div class="timeline" style="left: ${left}; width: ${width}; background-color: ${timeline.color};" data-timeline-id="${timeline.id}"></div>`
